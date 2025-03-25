2fadeb65256a08d17dafe814110499b9743add7d
Natalia Gruzdeva <natalia.gruzdeva@osome.com>
2025-03-25 17:18:14 +0300

    refactor db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { SequelizeModuleOptions } from '@nestjs/sequelize/dist/interfaces/sequelize-options.interface';
-import { Company } from '../db/models/Company';
-import { Ticket } from '../db/models/Ticket';
-import { User } from '../db/models/User';
+import { DbModule } from './db.module';
 import { TicketsController } from './tickets/tickets.controller';
-import dbConfig from '../db/config/config.json';
-
-console.log('dbConfig', dbConfig);
-
-const devConfig = dbConfig.development as SequelizeModuleOptions;
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      ...devConfig,
-      models: [Company, User, Ticket],
-    }),
-  ],
-})
-@Module({
-  imports: [],
+  imports: [DbModule],
   controllers: [TicketsController],
   providers: [],
 })
---
+++ src/db.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeModuleOptions } from '@nestjs/sequelize/dist/interfaces/sequelize-options.interface';
+import { Company } from '../db/models/Company';
+import { Ticket } from '../db/models/Ticket';
+import { User } from '../db/models/User';
+import dbConfig from '../db/config/config.json';
+
+const devConfig = dbConfig.development as SequelizeModuleOptions;
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      ...devConfig,
+      models: [Company, User, Ticket],
+    }),
+  ],
+})
+export class DbModule {}
