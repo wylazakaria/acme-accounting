2b30dec3db07cbfe7e98a33b51e53202ac1932db
Natalia Gruzdeva <natalia.gruzdeva@osome.com>
2025-03-25 15:56:00 +0300

    1st migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeModuleOptions } from '@nestjs/sequelize/dist/interfaces/sequelize-options.interface';
+import { Company } from '../db/models/company';
 import { TicketsController } from './tickets/tickets.controller';
 import dbConfig from '../db/config/config.json';
+
+console.log('dbConfig', dbConfig);
+
+const devConfig = dbConfig.development as SequelizeModuleOptions;
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
-      ...dbConfig,
-      models: [],
+      ...devConfig,
+      models: [Company],
     }),
   ],
 })
